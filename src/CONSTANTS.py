9f58d787326bc1a57716d3a45080570e9cc7cd2d
nathanyee <nathaniel.yee@students.olin.edu>
2017-07-13 10:35:10 -0700

    made directory create and video extractor

--- src/CONSTANTS.py
+++ src/CONSTANTS.py
@@ -1,18 +1,32 @@
 
 
-BASE_DIR     = '/Volumes/nyee/datasets/frcnn_data/'
-CLASSES      = ['BENTHOCODON','ECHINOCREPIS','PENIAGONE_SP_1','PENIAGONE_SP_A','PENIAGONE_VITRAE','SCOTOPLANES_GLOBOSA','ELPIDIA']
+BASE_DIR   = '/Volumes/nyee/datasets/frcnn_data/'
+SOURCE_DIR = '/Volumes/M3_temp/DeepLearningTests/benthic/'
 
-VID_NAMES  = ['D008_03HD', 'D0232_04HD', 'D0442_06HD', 'D0443_05HD', 'D0673_04HD', 'D0772_09HD']
-VID_DIRS   = ['{}{}/'.format(BASE_DIR, VID_NAME) for VID_NAME in VID_NAMES]
+CLASSES    = ['BENTHOCODON','ECHINOCREPIS','PENIAGONE_SP_1','PENIAGONE_SP_A','PENIAGONE_VITRAE','SCOTOPLANES_GLOBOSA','ELPIDIA']
+
 VID_EXT    = '.mov'
+IMG_EXT    = '.png'
+XML_EXT    = '.xml'
 
-IMG_DIR_NAME = 'imgs'
-IMG_DIRS     = ['{}{}/'.format(VID_DIR, IMG_DIR_NAME) for VID_DIR in VID_DIRS]
-IMG_EXT      = '.png'
+DIR_DICT   = {}
+VID_KEYS   = ['D008_03HD', 'D0232_04HD', 'D0442_06HD', 'D0443_05HD', 'D0673_04HD', 'D0772_09HD']
 
-XML_DIR_NAME = 'xmls'
-XML_DIRS     = ['{}{}/'.format(VID_DIR, XML_DIR_NAME) for VID_DIR in VID_DIRS]
-XML_EXT      = '.xml'
+# keys for the dict of each video
+VID_DIR    = 'video_dir'
+IMG_DIR    = 'imgs'
+XML_DIR    = 'xmls'
+DIR_KEYS   = [VID_DIR, IMG_DIR, XML_DIR]
 
-ANN_PATH     = BASE_DIR + 'simple_annotations.txt'
+SRC_VID    = 'source_video_path'
+
+# build DIR_DICT
+for VID in VID_KEYS:
+    DIR_DICT[VID]          = {}
+    DIR_DICT[VID][VID_DIR] = '{}{}/'.format(BASE_DIR, VID)
+    DIR_DICT[VID][IMG_DIR] = '{}{}/{}/'.format(BASE_DIR, VID, IMG_DIR)
+    DIR_DICT[VID][XML_DIR] = '{}{}/{}/'.format(BASE_DIR, VID, XML_DIR)
+    DIR_DICT[VID][SRC_VID] = '{}{}/{}{}'.format(SOURCE_DIR, VID, VID, VID_EXT)
+
+# resulting simple annotation for Keras frcnn
+ANN_PATH   = BASE_DIR + 'simple_annotations.txt'
